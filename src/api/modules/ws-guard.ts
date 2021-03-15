12eb86fd9d53e19a3b528beb6b5a870e6a688902
Your Name <you@example.com>
2021-03-15 18:28:28 +0000

    testing the tests

--- src/api/modules/ws-guard.ts
+++ src/api/modules/ws-guard.ts
@@ -8,7 +8,6 @@ import Channels from '../../data/channels';
 import { ChannelDocument } from '../../data/models/channel';
 import Roles from '../../data/roles';
 import { PermissionTypes } from '../../data/types/entity-types';
-import { Role } from '../../data/models/role';
 
 export class WSGuard {
   constructor(
@@ -21,21 +20,22 @@ export class WSGuard {
     return this.ws.sessions.get(client.id) ?? '';
   }
 
-  public validateIsUser(client: Socket, userId: string) {
+  public validateIsUser(client: Socket, userId: string) {    
     if (this.userId(client) !== userId)
       throw new TypeError('Unauthorized');
   }
 
   public async validateIsOwner(client: Socket, guildId: string) {
-    const member = await GuildMember.findOne({ guildId, ownerId: this.userId(client) });
+    const member = await GuildMember.findOne({
+      guildId,
+      ownerId: this.userId(client)
+    });
     if (!member)
       throw new TypeError('Only Guild Owner Can Do This');
   }
 
   public async canAccessChannel(client: Socket, channelId?: string) {
     const channel = await this.channels.get(channelId);
-    if (!channel) return false;
-
     const canAccess = await this.canAccess(channel, client);
     if (!canAccess)
       throw new TypeError('Missing Permissions');    
@@ -50,7 +50,7 @@ export class WSGuard {
   }
 
   public async can(client: Socket, guildId: string | undefined, permission: PermissionTypes.Permission) {
-    const userId = this.userId(client);
+    const userId = this.userId(client);    
     const member = await GuildMember.findOne({ guildId, userId });
     if (!member)
       throw new TypeError('Member Not Found');
@@ -58,7 +58,7 @@ export class WSGuard {
     const guild = await Guild.findById(guildId);
     if (!guild)
       throw new TypeError('Guild Not Found');
-
+    
     const can = this.roles.hasPermission(member, permission)
       || guild.ownerId === userId;
     if (!can)
