be031933cbf522d251411ea761c7c7523e43324b
Your Name <you@example.com>
2021-05-03 23:41:03 +0100

    update website

--- src/api/routes/users-routes.ts
+++ src/api/routes/users-routes.ts
@@ -42,7 +42,7 @@ router.get('/check-email', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const userCount = await User.countDocuments();
-  if (userCount >= 25)
+  if (userCount >= 35)
     throw new TypeError('Max alpha tester limit reached');
 
   const user = await users.create(req.body.username, req.body.password); 
