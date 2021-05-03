13d8760d1b54abae9306c652c0b2da39b019ae3e
Your Name <you@example.com>
2021-05-03 21:00:48 +0100

    fix embed images

--- src/api/server.ts
+++ src/api/server.ts
@@ -41,7 +41,7 @@ export class API {
     this.app.use(bodyParser.json());
     this.app.use(passport.initialize());
     this.app.use(cors());
-    this.app.use(helmet());
+    // this.app.use(helmet());
     this.app.use(rateLimiter);
   }
 
