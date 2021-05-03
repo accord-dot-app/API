5232816b6c96a66950d5a5f6143da9b608fdc44b
Your Name <you@example.com>
2021-05-03 16:38:15 +0100

    fix typing messages

--- src/api/websocket/ws-events/typing-start.ts
+++ src/api/websocket/ws-events/typing-start.ts
@@ -5,8 +5,11 @@ import { WSEvent, Args, Params, WSEventParams } from './ws-event';
 export default class implements WSEvent<'TYPING_START'> {
   on = 'TYPING_START' as const;
 
-  public async invoke(ws: WebSocket, client: Socket, { channelId }: Params.TypingStart) {
-    client.broadcast
+  public async invoke(ws: WebSocket, client: Socket, { channelId }: Params.TypingStart) {    
+    if (!client.rooms.has(channelId))
+      await client.join(channelId);
+    
+    ws.io
       .to(channelId)
       .emit('TYPING_START', {
         userId: ws.sessions.userId(client),
