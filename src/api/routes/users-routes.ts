6f46069c3bb327b62d34e2ab6e868ab7bf4a2607
= <adamjf123@gmail.com>
2021-05-11 18:25:31 +0100

    add 5 users

--- src/api/routes/users-routes.ts
+++ src/api/routes/users-routes.ts
@@ -42,7 +42,7 @@ router.get('/check-email', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const userCount = await User.countDocuments();
-  if (userCount >= 55)
+  if (userCount >= 60)
     throw new TypeError('Max alpha tester limit reached');
 
   const user = await users.create(req.body.username, req.body.password); 
