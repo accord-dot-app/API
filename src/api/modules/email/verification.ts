3c46323098cb8753fc5be8e8194d04336cdb815a
ADAMJR <adamjf123@gmail.com>
2021-06-19 19:50:23 +0100

    refactor login system

--- src/api/modules/email/verification.ts
+++ src/api/modules/email/verification.ts
@@ -3,13 +3,19 @@ import { generateInviteCode } from '../../../data/models/invite';
 export class Verification {
   private codes = new Map<string, VerifyCode>();
 
-  public create(email: string, type: VerifyCode['type'], expiresIn = 5 * 60 * 1000) {
+  public create(email: string, type: VerifyCode['type'], options?: EmailOptions) {
+    options = {
+      ...options,
+      codeLength: 16,
+      expiresIn: 5 * 60 * 1000,
+    };
+    
     this.codes.delete(email);
 
-    const value = generateInviteCode(16);
+    const value = generateInviteCode(options.codeLength);
     this.codes.set(email, { type, value });
 
-    setTimeout(() => this.codes.delete(email), expiresIn);
+    setTimeout(() => this.codes.delete(email), options.expiresIn);
 
     return value;
   }
@@ -31,4 +37,9 @@ export class Verification {
 interface VerifyCode {
   type: 'LOGIN' | 'VERIFY_EMAIL' | 'FORGOT_PASSWORD';
   value: string;
+}
+
+export interface EmailOptions {
+  codeLength?: number;
+  expiresIn?: number;
 }
